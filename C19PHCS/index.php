c4dd33269acf1961be5defa68aa42c04412d6bb9
Mohamed-Yasser Houssein <mike.012555@gmail.com>
2021-04-19 01:17:08 -0400

    added query links

--- C19PHCS/index.php
+++ C19PHCS/index.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>C19PHCS</title>
 </head>
-
 <body>
     <h1>Welcome!</h1>
     <p>Choose one of the following links:</p>
@@ -18,11 +17,21 @@
     <a href="./PHW/">Public Health Worker</a><br/>
     <a href="./GroupZone/">Group Zone</a><br/>
     <a href="./Alert-History/">Alert History</a><br/>
-    <a href="./Messages/">Messages</a><br/>
     <a href="./Form/">Symptom History Form</a><br/>
     <a href="./Recommendation/">Recommendation</a><br/>
-    <a href="./Symptom_History/">Symptom History</a><br/>
     <a href="./Tests/">Tests</a><br/>
+    <a href="./Alert-History/create.php ">query7</a><br/>
+    <a href="./Form/ ">query8</a><br/>
+    <a href="./Symptom_History/">Symptom History [query9]</a><br/>
+    <a href="./Symptom_History/person.php">query9</a><br/>
+    <a href="./Messages/">Messages [query 10]</a><br/>
+    <a href="./Person/find.php ">query11</a><br/>
+    <a href="./PHF/number.php">query12</a><br/>
+    <a href="./Region/detail.php">query13</a><br/>
+    <a href="./Tests/">query14</a><br/>
+    <a href="./Person/findByPHF.php">query15</a><br/>
+    <a href="./Tests/phw.php">query16</a><br/>
+    <a href="./Alert-History/regionalCases.php">query17</a><br/>
 </body>
 
 </html>
