5ec2d94192cf4ed70fd717b0c1bdab25b46cd0c4
mikey <mike.calixte@yahoo.ca>
2021-04-18 22:29:44 -0400

    fixed alert history creation

--- C19PHCS/Alert-History/create.php
+++ C19PHCS/Alert-History/create.php
@@ -2,17 +2,16 @@
 
 if (isset($_POST["region_name"])
 &&isset($_POST["alert_level"])
-&&isset($_POST["date_issued"])) {
-    $alertHistory = $conn->prepare("INSERT INTO $database.Alert_History (region_name, alert_level, date_issued, is_active) 
+&&isset($_POST["date_issued"])
+&&isset($_POST["is_active"])) {
+    $statement = $conn->prepare("INSERT INTO $database.Alert_History (region_name, alert_level, date_issued, is_active) 
                                                             VALUES (:region_name, :alert_level, :date_issued, :is_active);");
 
-    $alertHistory->bindParam(':region_name', $_POST["region_name"]);
-    $alertHistory->bindParam(':alert_level', $_POST["alert_level"]);
-    $alertHistory->bindParam(':date_issued', $_POST["date_issued"]);
-    $alertHistory->bindParam(':is_active', $_POST["is_active"]);
+    $statement->bindParam(':region_name', $_POST["region_name"]);
+    $statement->bindParam(':alert_level', $_POST["alert_level"]);
+    $statement->bindParam(':date_issued', $_POST["date_issued"]);
+    $statement->bindParam(':is_active', $_POST["is_active"]);
 
-    if ($alertHistory->execute())
-        header("Location: .");
 }
 
 ?>
@@ -24,7 +23,7 @@ if (isset($_POST["region_name"])
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Region</title>
+    <title>Add Alert History</title>
 </head>
 
 <body>
@@ -37,12 +36,12 @@ if (isset($_POST["region_name"])
         <input type="text" name="alert_level" id="alert_level"> <br>
 
         <label for="date_issued">Date Issued</label><br>
-        <input type="text" name="date_issued" id="date_issued"> <br>
+        <input type="date" name="date_issued" id="date_issued"> <br>
 
         <label for="is_active">Active</label><br>
         <input type="text" name="is_active" id="is_active"> <br>
         <br/>
-        <button type="submit">Add</button>
+        <input type="submit">
     </form>
     <a href="./">Back Region list</a>
 </body>
