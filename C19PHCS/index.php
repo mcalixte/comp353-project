c132ca2816f84468300305cb189be76ce600b14f
Mohamed-Yasser Houssein <mike.012555@gmail.com>
2021-04-19 10:04:41 -0400

    numbered list in menu

--- C19PHCS/index.php
+++ C19PHCS/index.php
@@ -16,20 +16,20 @@
     <a href="./Region/">4.Region</a><br/>
     <a href="./GroupZone/">5.Group Zone</a><br/>
     <a href="./Recommendation/">6.Recommendation</a><br/>
-    <a href="./Alert-History/create.php ">query7</a><br/>
-    <a href="./Form/ ">query8</a><br/>
-    <a href="./Symptom_History/">Symptom History [query9]</a><br/>
-    <a href="./Symptom_History/person.php">query9</a><br/>
-    <a href="./Messages/">Messages [query 10]</a><br/>
-    <a href="./Person/find.php ">query11</a><br/>
-    <a href="./PHF/number.php">query12</a><br/>
-    <a href="./Region/detail.php">query13</a><br/>
-    <a href="./Tests/">query14</a><br/>
-    <a href="./Person/findByPHF.php">query15</a><br/>
-    <a href="./Tests/phw.php">query16</a><br/>
-    <a href="./Alert-History/regionalCases.php">query17</a><br/>
+    <a href="./Alert-History/create.php ">7.Set a new alert</a><br/>
+    <a href="./Form/ ">8.Fill follow up form</a><br/>
+    <a href="./Symptom_History/person.php">9. Symptom progression</a><br/>
+    <a href="./Messages/">10.List messages within a period</a><br/>
+    <a href="./Person/find.php ">11.List people within an address</a><br/>
+    <a href="./PHF/number.php">12.List all facility details</a><br/>
+    <a href="./Region/detail.php">13.List all Region Details</a><br/>
+    <a href="./Tests/">14.List based on test results</a><br/>
+    <a href="./Person/findByPHF.php">15.List all PHWs in a facility</a><br/>
+    <a href="./Tests/phw.php">16.List positive PHWs</a><br/>
+    <a href="./Alert-History/regionalCases.php">17.Region </a><br/>
     <a href="./Alert-History/">Alert History</a><br/>
     <a href="./Tests/">Tests</a><br/>
+    <a href="./Symptom_History/">Symptom History</a><br/>
 </body>
 
 </html>
